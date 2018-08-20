72927be6ae1019e515758de656aa50b8735f4364
kennedyeduardo1996 <kennedyeduardomartins@gmail.com.br>
2018-08-20 17:01:50 -0300

    sucesso final

---
+++ unidade_05-criandoVariaveis/array2.php
@@ -0,0 +1,21 @@
+<?php
+//array tradicional
+ $salada = array("Maçã", "Banana", "Abacate");
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+    <body>
+    <?php
+        echo "funcionou";
+    ?>
+    <pre>
+        <?php
+
+        ?>
+    </pre>
+    </body>
+</html>
