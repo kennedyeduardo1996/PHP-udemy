68efc236d67b09bb89c5e688153785150c308610
kennedyeduardo1996 <kennedyeduardomartins@gmail.com.br>
2018-08-22 17:27:12 -0300

    trabalhando na unidade 11

--- unidade_11-formulario/destino.php
+++ unidade_11-formulario/destino.php
@@ -5,6 +5,18 @@
         <title>Curso PHP FUNDAMENTAL</title>
     </head>
 
-    <body> 
+    <body>
+    <pre>
+        <?php
+        print_r($_POST);
+
+        ?>
+    </pre>
+    <?php
+    $nome = isset($_POST["nome"])?$_POST["nome"]:"sem definição";
+    $email = isset($_POST["email"])?$_POST["email"]:"sem definição";
+    echo "Meu nome é ".$nome."<br>";
+    echo "Meu email é ".$email."<br>";
+    ?>
     </body>
 </html>
