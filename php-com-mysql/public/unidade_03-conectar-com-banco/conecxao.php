ed4ff9b78e69c79f8039c1ca39c3ec8399c91952
kennedyeduardo1996 <kennedyeduardomartins@gmail.com.br>
2018-08-23 17:29:41 -0300

    Iniciando

---
+++ php-com-mysql/public/unidade_03-conectar-com-banco/conecxao.php
@@ -0,0 +1,31 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "andes";
+$conexao = mysql_connect($servidor, $usuario, $senha, $banco);
+
+if(mysqli_connect_errno()){
+    die("Conexão falhou: " . mysqli_connect_errno());
+}
+
+$consulta_categorias = "SELECT * FROM categorias";
+$categorias = mysqli_query($conexao, $consulta_categorias);
+if(!$categorias){
+    die("Falha na consulta ao banco");
+}
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+    </body>
+    <?php
+    mysql_close($conexao);
+    ?>
+</html>
