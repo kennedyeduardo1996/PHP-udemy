78d23caca704a9b01bfa39d6760b76f8253773f4
kennedyeduardo1996 <kennedyeduardomartins@gmail.com.br>
2018-10-19 16:06:37 -0300

    php avancado upload

--- php-com-mysql/public/unidade_11-upload-de-arquivo/upload.php
+++ php-com-mysql/public/unidade_11-upload-de-arquivo/upload.php
@@ -1,25 +1,21 @@
 <?php require_once("../../conexao/conexao.php"); ?>
 <?php
-    $array_erro = array(
-        UPLOAD_ERR_OK => "Sem erro.",
-        UPLOAD_ERR_INI_SIZE => "O arquivo enviado excede o limite definido na diretiva upload_max_filesize do php.ini.",
-        UPLOAD_ERR_FORM_SIZE => "O arquivo excede o limite definido em MAX_FILE_SIZE no formulário HTML",
-        UPLOAD_ERR_PARTIAL => "O upload do arquivo foi feito parcialmente.",
-        UPLOAD_ERR_NO_FILE => "Nenhum arquivo foi enviado.",
-        UPLOAD_ERR_NO_TMP_DIR => "Pasta temporária ausente.",
-        UPLOAD_ERR_CANT_WRITE => "Falha em escrever o arquivo em disco.",
-        UPLOAD_ERR_EXTENSION => "Uma extensão do PHP interrompeu o upload do arquivo."
-    );
+
     if(isset($_POST['enviar'])) {
-        $numeroLinha = $_FILES['upload_file']['error'];
-        $mensagem = $array_erro[$numeroLinha];
 
-        echo "<pre>";
-            print_r($_FILES['upload_file']);
 
-        echo "</pre>";
+        $arquivo_temporario = $_FILES['upload_file']['tmp_name'];
+//        pega apenas e nome e não a extensao
+        $arquivo = basename($_FILES['upload_file']['name']);
+        $diretorio = 'upload';
 
-        echo "</hr>".$mensagem;
+        if (move_uploaded_file($arquivo_temporario, $diretorio.'/'.$arquivo)){
+            $mensagem = "Arquivo publicado";
+        }else{
+            $numeroLinha = $_FILES['upload_file']['error'];
+            $mensagem = $array_erro[$numeroLinha];
+        }
+
     }
 ?>
     <!doctype html>
@@ -49,6 +45,11 @@
             <input type="file" name="upload_file">
             <input type="submit" name="enviar" value="Enviar">
         </form>
+        <?php
+            if (isset($mensagem)){
+                echo $mensagem;
+            }
+        ?>
     </main>
 
     <?php
